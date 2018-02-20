44a4ebd892f24869e162eb192a276d0c1ed5f3c6
carlosblanco <carlosblancosierra@gmail.com>
2018-02-19 21:00:38 -0600

    Initial commit Lesson 33

---
+++ ecommerce/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/carlosblanco/Documents/Dev/Ecommerce-CodingForEntrepeneurs/ecommerce/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ ecommerce/lib/python3.6/abc.py
@@ -0,0 +1,1 @@
+/Users/carlosblanco/anaconda3/lib/python3.6/abc.py
