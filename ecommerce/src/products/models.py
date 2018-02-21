4ecae49b30697bc7d0e78b049ae5eadffaa9ce98
carlosblanco <carlosblancosierra@gmail.com>
2018-02-21 13:12:49 -0600

    37. Reverse for URLs

--- ecommerce/src/products/models.py
+++ ecommerce/src/products/models.py
@@ -3,6 +3,8 @@ import os
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 from .utils import unique_slug_generator
+from django.urls import reverse
+
 
 def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
@@ -57,7 +59,8 @@ class Product(models.Model):
     objects= ProductManager()
 
     def get_absolute_url(self):
-        return "/products/{slug}/".format(slug=self.slug)
+        #return "/products/{slug}/".format(slug=self.slug)
+        return reverse("products:detail", kwargs={"slug": self.slug})
 
     def __str__(self):
         return self.title
