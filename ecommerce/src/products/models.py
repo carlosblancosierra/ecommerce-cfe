d97e649615db379bd5f6b23fd43dbe2bae818fb5
carlosblanco <carlosblancosierra@gmail.com>
2018-02-27 15:50:16 -0600

    56. Better Lookups with Q

--- ecommerce/src/products/models.py
+++ ecommerce/src/products/models.py
@@ -1,5 +1,6 @@
 import random
 import os
+from django.db.models import Q
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 from .utils import unique_slug_generator
@@ -30,6 +31,12 @@ class ProductQuerySet(models.query.QuerySet):
     def featured(self):
         return self.filter(featured=True, active=True)
 
+    def search(self, query):
+        lookups = (Q(title__icontains=query) |
+            Q(description__icontains=query) |
+            Q(price__icontains=query))
+        return self.filter(lookups).distinct()
+
 class ProductManager(models.Manager):
     def get_queryset(self):
         return ProductQuerySet(self.model, using=self._db)
@@ -45,6 +52,11 @@ class ProductManager(models.Manager):
         if qs.count() == 1:
             return qs.first()
         return None
+
+    def search(self, query):
+        lookups = Q(title__icontains=query) | Q(description__icontains=query)
+        return self.get_queryset().active().filter(lookups).distinct()
+
 
 # Create your models here.
 class Product(models.Model):
