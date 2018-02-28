4edd41e76c646046c5ea39dae165d75192b302db
carlosblanco <carlosblancosierra@gmail.com>
2018-02-27 19:22:39 -0600

    65. Cart Model Manager Part 1

--- ecommerce/src/carts/views.py
+++ ecommerce/src/carts/views.py
@@ -10,21 +10,17 @@ def cart_create(user=None):
 # Create your views here.
 def cart_home(request):
 
-
     cart_id = request.session.get("cart_id", None)
 
-    if cart_id is None:
-        #print("create new cart")
-        cart_obj = cart_create()
-        request.session['cart_id'] = cart_obj.id
+    qs = Cart.objects.filter(id=cart_id)
+    if qs.count() == 1:
+        cart_obj = qs.first()
+        print("Cart ID exists")
+        if request.user.is_authenticated and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
     else:
-        qs = Cart.objects.filter(id=cart_id)
-        if qs.count() == 1:
-            cart_obj = qs.first()
-            print("Cart ID exists")
-        else:
-            cart_obj = cart_create()
-            request.session['cart_id'] = cart_obj.id
-
+        cart_obj = Cart.objects.new(user=request.user)
+        request.session['cart_id'] = cart_obj.id
 
     return render(request, "carts/home.html", {})
