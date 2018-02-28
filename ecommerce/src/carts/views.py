22c43d6e33a4e48f204f8bb7e7ebeb80991ccf84
carlosblanco <carlosblancosierra@gmail.com>
2018-02-27 20:12:31 -0600

    67. M2M Changed Signal to Calculate Cart Total

--- ecommerce/src/carts/views.py
+++ ecommerce/src/carts/views.py
@@ -9,18 +9,17 @@ def cart_create(user=None):
 
 # Create your views here.
 def cart_home(request):
-
-    cart_id = request.session.get("cart_id", None)
-
-    qs = Cart.objects.filter(id=cart_id)
-    if qs.count() == 1:
-        cart_obj = qs.first()
-        print("Cart ID exists")
-        if request.user.is_authenticated and cart_obj.user is None:
-            cart_obj.user = request.user
-            cart_obj.save()
-    else:
-        cart_obj = Cart.objects.new(user=request.user)
-        request.session['cart_id'] = cart_obj.id
+    cart_obj = Cart.objects.new_or_get(request)
+    # cart_id = request.session.get("cart_id", None)
+    # qs = Cart.objects.filter(id=cart_id)
+    # if qs.count() == 1:
+    #     cart_obj = qs.first()
+    #     print("Cart ID exists")
+    #     if request.user.is_authenticated and cart_obj.user is None:
+    #         cart_obj.user = request.user
+    #         cart_obj.save()
+    # else:
+    #     cart_obj = Cart.objects.new(user=request.user)
+    #     request.session['cart_id'] = cart_obj.id
 
     return render(request, "carts/home.html", {})
