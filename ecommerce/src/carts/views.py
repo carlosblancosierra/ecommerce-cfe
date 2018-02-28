4a2a78362de7bf1566cc62e85b7bf94e9cf4a71e
carlosblanco <carlosblancosierra@gmail.com>
2018-02-27 18:45:10 -0600

    63. Cart Model

--- ecommerce/src/carts/views.py
+++ ecommerce/src/carts/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def cart_home(request):
+
+    cart_id = request.session.get("cart_id", None)
+    if cart_id is None: # and isintance(cart_id, int):
+        print("create new cart")
+        request.session['cart_id'] = 12
+    else:
+        print("Cart ID exists")
+
+
     return render(request, "carts/home.html", {})
