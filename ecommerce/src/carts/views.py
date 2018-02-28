42e2763096270befff5c6843a59fb54c4bbf5ed4
carlosblanco <carlosblancosierra@gmail.com>
2018-02-27 21:37:49 -0600

    70. Display Cart

--- ecommerce/src/carts/views.py
+++ ecommerce/src/carts/views.py
@@ -2,28 +2,14 @@ from django.shortcuts import render, redirect
 from products.models import Product
 from .models import Cart
 
-def cart_create(user=None):
-    cart_obj = Cart.objects.create(user=None)
-    print("new cart created")
-    return cart_obj
+# def cart_create(user=None):
+#     cart_obj = Cart.objects.create(user=None)
+#     print("new cart created")
+#     return cart_obj
 
-
-# Create your views here.
 def cart_home(request):
-    cart_obj = Cart.objects.new_or_get(request)
-    # cart_id = request.session.get("cart_id", None)
-    # qs = Cart.objects.filter(id=cart_id)
-    # if qs.count() == 1:
-    #     cart_obj = qs.first()
-    #     print("Cart ID exists")
-    #     if request.user.is_authenticated and cart_obj.user is None:
-    #         cart_obj.user = request.user
-    #         cart_obj.save()
-    # else:
-    #     cart_obj = Cart.objects.new(user=request.user)
-    #     request.session['cart_id'] = cart_obj.id
-
-    return render(request, "carts/home.html", {})
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    return render(request, "carts/home.html", {"cart":cart_obj})
 
 def cart_update(request):
     product_id = request.POST.get('product_id')
