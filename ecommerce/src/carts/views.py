7368cb5b874fd205746df56255ce24c8d8ec6217
carlosblanco <carlosblancosierra@gmail.com>
2018-02-28 21:08:02 -0600

    78. Checkout View

--- ecommerce/src/carts/views.py
+++ ecommerce/src/carts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+
+from orders.models import Order
 from products.models import Product
 from .models import Cart
 
@@ -10,6 +12,7 @@ from .models import Cart
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
     return render(request, "carts/home.html", {"cart":cart_obj})
+
 
 def cart_update(request):
     product_id = request.POST.get('product_id')
@@ -32,3 +35,14 @@ def cart_update(request):
     #cart_ob.products.add(product_id)
     #return redirect(product_obj.get_absolute_url())
     return redirect("cart:home")
+
+
+def checkout_home(request):
+    cart_obj, cart_created = Cart.objects.new_or_get(request)
+    order_obj = None
+
+    if cart_created or cart_obj.products.count() == 0:
+        return redirect("cart:home")
+    else:
+        order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
+    return render(request, "carts/checkout.html", {"object":order_obj})
