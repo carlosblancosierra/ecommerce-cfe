638013457f6402fab6f02cfac4d5ee888a7cec93
carlosblanco <carlosblancosierra@gmail.com>
2018-03-08 12:55:12 -0600

    114. Ajaxify the Contact Form Part 2

--- ecommerce/src/ecommerce/forms.py
+++ ecommerce/src/ecommerce/forms.py
@@ -33,11 +33,14 @@ class ContactForm(forms.Form):
             )
         )
 
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        if not "gmail.com" in email:
-            raise forms.ValidationError("Email has to be gmail")
-        return email
+    # def clean_email(self):
+    #     email = self.cleaned_data.get("email")
+    #     if not "gmail.com" in email:
+    #         raise forms.ValidationError("Email has to be gmail")
+    #     return email
+
+    # def clean_content(self):
+    #     raise forms.ValidationError("Email has to be gmail")
 
 
 
