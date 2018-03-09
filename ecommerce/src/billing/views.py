2b71a3d4153efb32a592205657751ff273818caa
carlosblanco <carlosblancosierra@gmail.com>
2018-03-09 14:39:13 -0600

    143. Save Card in Django

--- ecommerce/src/billing/views.py
+++ ecommerce/src/billing/views.py
@@ -2,13 +2,12 @@ from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
 
-
 import stripe
 
 stripe.api_key = 'sk_test_x33KBxusHfdA7eXMyaHdJp6i'
 STRIPE_PUB_KEY = 'pk_test_oXY7CAW0bxemHlmDIvnpw4TB'
 
-from .models import BillingProfile
+from .models import BillingProfile, Card
 
 def payment_method_view(request):
 
@@ -34,7 +33,8 @@ def payment_method_createview(request):
         if token is not None:
             customer = stripe.Customer.retrieve(billing_profile.customer_id)
             card_response = customer.sources.create(source=token)
-            print(card_response)
+            new_card_objects = Card.objects.add_new(billing_profile, card_response)
+            print(new_card_objects) #start saving our cards too!
 
         print(request.POST)
         return JsonResponse({"message":"Success! Your card was added"})
